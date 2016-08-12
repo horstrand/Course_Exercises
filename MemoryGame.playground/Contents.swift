ebd2fa7796683b75b7da9df387b064bb322aeffc
Pablo Horstrand Andaluz <pablohorstrandandaluz@macbook-pro-de-pablo-horstrand-andaluz.local>
2016-08-12 21:06:10 +0200

    Finish exercise code

--- MemoryGame.playground/Contents.swift
+++ MemoryGame.playground/Contents.swift
@@ -6,6 +6,13 @@ import UIKit
 var range = 0...100
 
 for idx in range{
-    
-    
+    if idx % 5 == 0{
+        print("\(idx) Bingo!!!")
+    }else if idx % 2 == 0{
+        print("\(idx) par!!!")
+    }else if idx % 2 == 1{
+        print("\(idx) impar!!!")
+    }else if idx >= 30 && idx <= 40{
+        print("\(idx) Viva Swift!!!")
+    }
 }
