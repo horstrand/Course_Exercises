dae8cc164d8d6f56bb2010d4620dae918fe76acc
Pablo Horstrand Andaluz <pablohorstrandandaluz@macbook-pro-de-pablo-horstrand-andaluz.local>
2016-08-12 21:22:58 +0200

    Modified to print more than one condition for a number

--- MemoryGame.playground/Contents.swift
+++ MemoryGame.playground/Contents.swift
@@ -8,11 +8,13 @@ var range = 0...100
 for idx in range{
     if idx % 5 == 0{
         print("\(idx) Bingo!!!")
-    }else if idx % 2 == 0{
+    }
+    if idx % 2 == 0{
         print("\(idx) par!!!")
-    }else if idx % 2 == 1{
+    }else{
         print("\(idx) impar!!!")
-    }else if idx >= 30 && idx <= 40{
+    }
+    if idx >= 30 && idx <= 40{
         print("\(idx) Viva Swift!!!")
     }
 }
